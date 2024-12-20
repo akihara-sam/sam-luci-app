3ea708ceda4d5a11c7cceadc80216ccd9a4f52e7
akihara-sam <92433496+akihara-sam@users.noreply.github.com>
2024-12-20 18:24:48 +0800

    Update settings.lua

--- luci-app-pppwn/luasrc/model/cbi/pppwn/settings.lua
+++ luci-app-pppwn/luasrc/model/cbi/pppwn/settings.lua
@@ -8,12 +8,14 @@ s = m:section(TypedSection, "pppwn")
 s.addremove = false
 s.anonymous = true
 
-enable=s:option(Flag, "enable", translate("Enabled"))
+enable = s:option(Flag, "enable", translate("Enabled"))
 enable.rmempty = false
 
 -- 设置启用/禁用功能
 function enable.write(self, section, value)
     Flag.write(self, section, value)
+    local uci = require "luci.model.uci".cursor()
+    uci:commit("pppwn") -- 立即提交配置更改
 
     if value == "1" then
         -- 启动 PPPwn
@@ -24,7 +26,7 @@ function enable.write(self, section, value)
     end
 end
 
-source=s:option(Value, "source", translate("PPPwn Running Port"))
+source = s:option(Value, "source", translate("PPPwn Running Port"))
 source.datatype = "network"
 source.default = "br-lan"
 source.rmempty = false
@@ -32,7 +34,7 @@ for _, e in ipairs(sys.net.devices()) do
     if e ~= "lo" then source:value(e) end
 end
 
-fwver=s:option(Value, "fwver", translate("PlayStation 4 System Version"))
+fwver = s:option(Value, "fwver", translate("PlayStation 4 System Version"))
 fwver.default = "1100"
 fwver.rmempty = false
 fwver:value("700", translate("7.00"))
@@ -43,17 +45,15 @@ fwver.description = translate("1100 means Ver 11.00 etc.")
 goldhen = s:option(DummyValue, "goldhen", translate("Download GoldHEN Payload BIN"))
 goldhen.description = translate("<a class='btn cbi-button cbi-button-apply' type='button' href=\"../../../../goldhen.bin\" target=\"_blank\" />"..translate("Copy goldhen.bin to the root directory of an exfat/fat32 USB and insert it into your PS4").."</a>")
 
--- 定义 apply 方法以处理保存并应用操作
+-- 强制在“保存并应用”时立即启停服务
 function m.apply(self)
     local uci = require "luci.model.uci".cursor()
     local enabled = uci:get("pppwn", "pppwn", "enable")
 
     if enabled == "1" then
-        -- 启动服务
         luci.sys.call("/etc/init.d/pppwn start")
         luci.sys.call("logger -t PPPwn 'Service started after apply'")
     else
-        -- 停止服务
         luci.sys.call("/etc/init.d/pppwn stop")
         luci.sys.call("logger -t PPPwn 'Service stopped after apply'")
     end
