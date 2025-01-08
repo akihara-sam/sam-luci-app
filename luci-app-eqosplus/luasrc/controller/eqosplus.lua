6e0b8f039aa6552fd05170ac7a0430789d500a56
akihara-sam <sam790302@163.com>
2025-01-08 13:59:14 +0800

    Update luci-app-eqosplus: Moved to Network menu and optimized code

--- luci-app-eqosplus/luasrc/controller/eqosplus.lua
+++ luci-app-eqosplus/luasrc/controller/eqosplus.lua
@@ -1,12 +1,15 @@
 module("luci.controller.eqosplus", package.seeall)
 -- Copyright 2022-2023 sirpdboy <herboy2008@gmail.com>
+
 function index()
     if not nixio.fs.access("/etc/config/eqosplus") then return end
-    entry({"admin", "network"}, firstchild(), "Network", 40).dependent = false
 
-    local e = entry({"admin", "network", "eqosplus"}, cbi("eqosplus"), _( "Eqosplus"), 10)
-    e.dependent=false
+    -- 将 eqosplus 菜单项添加到 network 菜单下，确保它位于网络菜单下的指定位置
+    local e = entry({"admin", "network", "eqosplus"}, cbi("eqosplus"), _("Eqosplus"), 40)
+    e.dependent = false
     e.acl_depends = { "luci-app-eqosplus" }
+
+    -- 添加一个状态页菜单项，位于 eqosplus 菜单下
     entry({"admin", "network", "eqosplus", "status"}, call("act_status")).leaf = true
 end
 
@@ -17,3 +20,4 @@ function act_status()
     luci.http.prepare_content("application/json")
     luci.http.write_json(e)
 end
+
